d62d93d266d1c1319af4a303bf373bfbd8e680b0
Najeeb <playitforpubg@gmail.com>
2025-08-06 04:06:18 +0530

    LUKA V1-10%

---
+++ script.js
@@ -0,0 +1,13 @@
+var score=0
+const lukaBox = luka.getBoundingClientRect();
+const fishBox = fish.getBoundingClientRect();
+
+ function lukacollectfish(){
+    while(lukaBox>fishBox){
+        const lukaEl = document.querySelector('.luka');
+const fishEls = Array.from(document.querySelectorAll('.fish'));
+        lukaEl=setInterval(30,60)
+
+
+    }
+ }
